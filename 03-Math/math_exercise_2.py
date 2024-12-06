fd686410936b7ae23bbdc99f99918a6d578cbbc2
Anna Fomina <ann.fomi77@gmail.com>
2024-12-06 14:36:20 +0300

    add second file with math problem functions

---
+++ 03-Math/math_exercise_2.py
@@ -0,0 +1,37 @@
+"""Math exercises vol2."""
+import math
+
+
+def time_converter(seconds: int) -> str:
+    """Convert time in seconds to hours and minutes."""
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    return f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it)."
+
+
+def student_helper(angle: int) -> str:
+    """Return the sine and cosine of the given angle in degrees."""
+    radians = math.radians(angle)
+    sine = round(math.sin(radians), 1)
+    cosine = round(math.cos(radians), 1)
+    return f"Nurk: {angle}, siinus: {sine}, koosinus: {cosine}."
+
+
+def greetings(n: int) -> str:
+    """Return a string that contains "Hey" n times."""
+    lots_of_heys = "Hey" * n
+    return lots_of_heys
+
+
+def adding_numbers(num_a: int, num_b: int) -> str:
+    """Return given numbers added together as a string."""
+    string_numbers = str(num_a) + str(num_b)
+    return string_numbers
+
+
+if __name__ == "__main__":
+    assert time_converter(57000)
+    assert student_helper(45)
+    assert greetings(100)
+    assert adding_numbers(100, 100)
+    print("OK!")
